d9254c21fd3e800f056556c1b745f92c80f80e0e
kotenkoroman2019 <kotenkoroman2019@gmail.com>
2020-08-25 20:27:49 +0300

    change components styles

--- src/components/PassengerFlight.js
+++ src/components/PassengerFlight.js
@@ -9,15 +9,15 @@ const PassengerFlight = ({ flights }) => {
       <h4>Passenger: <span>{flights.user}</span></h4>
 
       <h6>
-        Flight &#35;: 
+        Flight &#35;:{' '}
           <span style={{marginRight: 25, color: 'green'}}>{flights.flight_no}</span> 
         Status: <span style={{color: 'green'}}>{flights.status}</span>
       </h6>
 
-      <div>
+      <div className="wrapper">
         <div>From City: <span className="city">{flights.from_city}</span></div>
         <div>Airport: <span>{flights.from}</span></div>
-        <div style={{marginBottom: 5}}>
+        <div>
           Departure:{' '}
             <span>
               <Moment format="MM/DD HH:mm" unix>
@@ -27,10 +27,10 @@ const PassengerFlight = ({ flights }) => {
           </div>
       </div>
 
-      <div>
+      <div className="wrapper">
         <div>To City: <span  className="city">{flights.to_city}</span></div>
         <div>Airport: <span>{flights.to}</span></div>
-        <div style={{marginBottom: 5}}>
+        <div>
           Arrival:{' '}
             <span>
               <Moment format="MM/DD HH:mm" unix>
